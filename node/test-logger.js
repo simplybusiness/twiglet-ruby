299f3587e57f79b4d3f8494a51549c66b092ba28
Quynh Nguyen <quynh.nguyen@simplybusiness.co.uk>
2020-05-07 13:46:35 +0100

    Fix the tests

--- node/test-logger.js
+++ node/test-logger.js
@@ -30,25 +30,26 @@ describe("logging", () => {
   it("should log mandatory attributes", () => {
     this.log.error({})
     const contents = this.log.output.printed
-
     expect(contents.timestamp).toBe("2016-02-15T12:34:56.789Z")
-    expect(contents["service.name"]).toBe("my-super-service")
-    expect(contents["log.level"]).toBe("ERROR")
+    expect(contents.service.name).toBe("my-super-service")
+    expect(contents.log.level).toBe("ERROR")
   })
 
   it("should log the provided message", () => {
-    this.log.error({ "event.action": "exception", message: "Emergency! Emergency!" })
+    this.log.error({ event: { action: "exception" }, message: "Emergency! Emergency!" })
     const contents = this.log.output.printed
 
-    expect(contents["event.action"]).toBe("exception")
+    expect(contents.event.action).toBe("exception")
     expect(contents.message).toBe("Emergency! Emergency!")
   })
 
   it("should log scoped properties defined at creation", () => {
     const extra_properties = {
-      "trace.id": "126bb6fa-28a2-470f-b013-eefbf9182b2d",
+      trace: {
+        id: "1c8a5fb2-fecd-44d8-92a4-449eb2ce4dcb"
+      },
       request: { method: "GET" },
-      response: { status: 200 }
+      response: { status_code: 200 }
     }
     const my_logger = Logger({
       now: () => "2016-02-15T12:34:56.789Z",
@@ -59,8 +60,8 @@ describe("logging", () => {
     my_logger.error(extra_properties)
     const contents = my_logger.output.printed
 
-    expect(contents["trace.id"]).toBe("126bb6fa-28a2-470f-b013-eefbf9182b2d")
+    expect(contents.trace.id).toBe("1c8a5fb2-fecd-44d8-92a4-449eb2ce4dcb")
     expect(contents.request.method).toBe("GET")
-    expect(contents.response.status).toBe(200)
+    expect(contents.response.status_code).toBe(200)
   })
 })
