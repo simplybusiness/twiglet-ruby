01413b003cf834a7b93fd3ae98d59060386d3e51
Quynh Nguyen <quynhnh@gmail.com>
2020-05-08 11:25:24 +0100

    Tests to enforce a message

--- node/test-logger.js
+++ node/test-logger.js
@@ -1,5 +1,4 @@
 // Dependencies
-// const jasmine = require("jasmine")
 const Logger = require("./logger")
 
 // Constants
@@ -89,9 +88,27 @@ describe("logging", () => {
   })
 
   describe("enforcing non-empty message", () => {
-    it("should throws on error on an empty message", () => {
+    it("should throws on error without a message", () => {
       expect(() => {
         this.log.info()
+      }).toThrow()
+    })
+
+    it("should throws on error on an empty message", () => {
+      expect(() => {
+        this.log.info("")
+      }).toThrow()
+    })
+
+    it("should throws on error on a null message", () => {
+      expect(() => {
+        this.log.info(null)
+      }).toThrow()
+    })
+
+    it("should throws on error on an undefined message", () => {
+      expect(() => {
+        this.log.info(undefined)
       }).toThrow()
     })
   })
