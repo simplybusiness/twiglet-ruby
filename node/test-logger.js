a992c81dc207347458ef95b862f243ab151eac5a
Quynh Nguyen <mojitocoder@users.noreply.github.com>
2020-05-11 10:32:50 +0100

    Update node/test-logger.js
    Co-authored-by: @sleepyfox <sleepyfox@gmail.com>

--- node/test-logger.js
+++ node/test-logger.js
@@ -48,7 +48,7 @@ describe("logging", () => {
       trace: {
         id: "1c8a5fb2-fecd-44d8-92a4-449eb2ce4dcb"
       },
-      message: "Cats department visited",
+      message: "GET /cats",
       request: { method: "GET" },
       response: { status_code: 200 }
     }
