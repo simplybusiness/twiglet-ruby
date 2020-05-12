c08ff99e65fd0bcd714335eeef24ef1eae622375
Quynh Nguyen <mojitocoder@users.noreply.github.com>
2020-05-12 11:12:18 +0100

    Logger in Ruby (#9)
    * Gemfile
    
    * WIP: logger in Ruby
    
    * Gemfile
    
    * WIP: logger in Ruby
    
    * Use unamed params
    
    * Downcase log.level
    
    * Add Minitest
    
    * Log level
    
    * First 2 test cases
    
    * More test cases
    
    * All tests passed
    
    * Work with message provided in string key of a Hash
    
    * Update ruby/logger.rb
    
    Co-authored-by: John Gallagher <john.gallagher@simplybusiness.co.uk>
    
    * Update ruby/logger.rb
    
    Co-authored-by: John Gallagher <john.gallagher@simplybusiness.co.uk>
    
    * Update ruby/test/logger_test.rb
    
    Co-authored-by: John Gallagher <john.gallagher@simplybusiness.co.uk>
    
    * Update ruby/test/logger_test.rb
    
    Co-authored-by: John Gallagher <john.gallagher@simplybusiness.co.uk>
    
    * Update ruby/test/logger_test.rb
    
    Co-authored-by: John Gallagher <john.gallagher@simplybusiness.co.uk>
    
    * Update ruby/test/logger_test.rb
    
    Co-authored-by: John Gallagher <john.gallagher@simplybusiness.co.uk>
    
    * Update ruby/test/logger_test.rb
    
    Co-authored-by: John Gallagher <john.gallagher@simplybusiness.co.uk>
    
    * Update ruby/test/logger_test.rb
    
    Co-authored-by: John Gallagher <john.gallagher@simplybusiness.co.uk>
    
    * Update ruby/test/logger_test.rb
    
    Co-authored-by: John Gallagher <john.gallagher@simplybusiness.co.uk>
    
    * Fix tests and change variable names
    
    * Remove placeholder
    
    Co-authored-by: John Gallagher <john.gallagher@simplybusiness.co.uk>

---
+++ ruby/logger.rb
@@ -0,0 +1,71 @@
+require 'date'
+require 'json'
+
+class Logger
+  def initialize(service:, now:, output:, scoped_properties: {})
+    @service = service
+    @now = now
+    @output = output
+    @scoped_properties = scoped_properties
+  end
+
+  def debug(message)
+    log(level: "debug", message: message)
+  end
+
+  def info(message)
+    log(level: "info", message: message)
+  end
+
+  def warning(message)
+    log(level: "warning", message: message)
+  end
+
+  def error(message)
+    log(level: "error", message: message)
+  end
+
+  def critical(message)
+    log(level: "critical", message: message)
+  end
+
+  def with(scoped_properties)
+    Logger.new(service: @service,
+               now: @now,
+               output: @output,
+               scoped_properties: scoped_properties)
+  end
+
+  private
+
+  def valid_string?(message)
+    message.strip.length > 0
+  end
+
+  def log(level:, message:)
+    case message
+    when String
+      raise "There must be a non-empty message" unless valid_string?(message)
+      message = { message: message }
+    when Hash
+      message = message.transform_keys(&:to_sym)
+      raise "Log object must have a 'message' property" unless message.key?(:message)
+      raise "The 'message' property of log object must not be empty" unless valid_string?(message[:message])
+    else
+      raise "Message must be either an object or a string"
+    end
+
+    total_message = ({
+        service: {
+          name: @service
+        },
+        "@timestamp": @now.iso8601(3),
+        log: {
+          level: level
+        }
+      })
+      .merge(@scoped_properties)
+      .merge(message)
+    @output.puts total_message.to_json
+  end
+end
---
+++ ruby/test/logger_test.rb
@@ -0,0 +1,114 @@
+require "minitest/autorun"
+require_relative "../logger"
+
+describe Logger do
+  before do
+    @now = Time.local(2020, 5, 11, 15, 1, 1).to_datetime
+    @buffer = StringIO.new
+    @logger = Logger.new(
+      service: "petshop",
+      now: @now,
+      output: @buffer
+    )
+  end
+
+  it "should throw an error with an empty message" do
+    assert_raises RuntimeError do
+      @logger.info("")
+    end
+  end
+
+  it "should log mandatory attributes" do
+    @logger.error("Out of pets exception")
+    actual_log = read_json(@buffer)
+
+    expected_log = {
+      message: "Out of pets exception",
+      "@timestamp": "2020-05-11T15:01:01.000+01:00",
+      service: {
+        name: "petshop"
+      },
+      log: {
+        level: "error"
+      }
+    }
+
+    assert_equal expected_log, actual_log
+  end
+
+  it "should log the provided message" do
+    @logger.error({ event:
+                      { action: "exception" },
+                    message: "Emergency! Emergency!"
+                  })
+    log = read_json(@buffer)
+
+    assert_equal "exception", log[:event][:action]
+    assert_equal "Emergency! Emergency!", log[:message]
+  end
+
+  it "should log scoped properties defined at creation" do
+    extra_properties = {
+      trace: {
+        id: "1c8a5fb2-fecd-44d8-92a4-449eb2ce4dcb"
+      },
+      request: { method: "get" },
+      response: { status_code: 200 }
+    }
+
+    output = StringIO.new
+    logger = Logger.new(
+      service: "petshop",
+      now: @now,
+      output: output,
+      scoped_properties: extra_properties
+    )
+
+    logger.error("GET /cats")
+    log = read_json output
+
+    assert_equal "1c8a5fb2-fecd-44d8-92a4-449eb2ce4dcb", log[:trace][:id]
+    assert_equal "get", log[:request][:method]
+    assert_equal 200, log[:response][:status_code]
+  end
+
+  it "should be able to add properties with '.with'" do
+    # Let's add some context to this customer journey
+    purchase_logger = @logger.with({
+      trace: { id: "1c8a5fb2-fecd-44d8-92a4-449eb2ce4dcb" },
+      customer: { full_name: "Freda Bloggs" },
+      event: { action: "pet purchase" }
+    })
+
+    # do stuff
+    purchase_logger.info({
+      message: "customer bought a dog",
+      pet: { name: "Barker", species: "dog", breed: "Bitsa" }
+    })
+
+    log = read_json @buffer
+
+    assert_equal "1c8a5fb2-fecd-44d8-92a4-449eb2ce4dcb", log[:trace][:id]
+    assert_equal "Freda Bloggs", log[:customer][:full_name]
+    assert_equal "pet purchase", log[:event][:action]
+    assert_equal "customer bought a dog", log[:message]
+    assert_equal "Barker", log[:pet][:name]
+  end
+
+  it "should log 'message' property" do
+    message = {}
+    message["message"] = "Guinea pig arrived"
+    @logger.debug(message)
+    log = read_json(@buffer)
+
+    assert_equal "Guinea pig arrived", log[:message]
+  end
+
+  private
+
+  def read_json(buffer)
+    buffer.rewind
+    JSON.parse(buffer.read, symbolize_names: true)
+  end
+
+end
