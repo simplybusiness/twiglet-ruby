f9942bdbca59973c03826c341204956c810efac2
Nigel Runnels-Moss <sleepyfox@gmail.com>
2020-07-16 13:32:31 +0100

    Fix whitespace

--- test/test_coverage.rb
+++ test/test_coverage.rb
@@ -3,7 +3,7 @@ require 'simplecov'
 SimpleCov.start do
   add_filter "/test/"
   add_filter "examples/rack/request_logger_test.rb"
-  
+
   if ENV['CI']
     formatter SimpleCov::Formatter::SimpleFormatter
   else
