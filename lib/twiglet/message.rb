605e76beffe4b5fbdc6a9a4dfd518bde39471093
Nicole Rifkin <rifkinni@users.noreply.github.com>
2020-08-27 12:29:56 -0400

    Message class (#12)
    * create an isolated message class that extends hash
    
    * clean up error logging
    
    * unit test
    
    * version

---
+++ lib/twiglet/message.rb
@@ -0,0 +1,24 @@
+module Twiglet
+  class Message < Hash
+    def initialize(msg)
+      case msg
+      when String
+        self[:message] = msg
+      when Hash
+        replace(msg.transform_keys!(&:to_sym))
+      end
+
+      validate!
+    end
+
+    private
+
+    def validate!
+      raise 'Message must be initialized with a String or a non-empty Hash' if empty?
+
+      raise 'Log object must have a \'message\' property' unless self[:message]
+
+      raise 'The \'message\' property of the log object must not be empty' if self[:message].strip.empty?
+    end
+  end
+end
--- lib/twiglet/version.rb
+++ lib/twiglet/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module Twiglet
-  VERSION = '2.3.10'
+  VERSION = '2.3.11'
 end
