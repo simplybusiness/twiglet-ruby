e0034108546195ae1695ec73ce8ae3fbebcbb142
Nicole Rifkin <nicole.rifkin@simplybusiness.com>
2020-08-27 10:16:16 -0400

    add to validation method

--- lib/twiglet/message.rb
+++ lib/twiglet/message.rb
@@ -6,8 +6,6 @@ module Twiglet
         self[:message] = msg
       when Hash
         replace(msg.transform_keys!(&:to_sym))
-      else
-        raise('Message must be String or Hash')
       end
 
       validate!
@@ -16,6 +14,8 @@ module Twiglet
     private
 
     def validate!
+      raise 'Message must be initialized with a String or a non-empty Hash' if empty?
+
       message = fetch(:message) { raise('Log object must have a \'message\' property') }
       raise('The \'message\' property of the log object must not be empty') if message.strip.empty?
     end
