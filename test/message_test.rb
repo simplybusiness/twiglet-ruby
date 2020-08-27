b5044c912faca92b61e99cdf71d4549841d1d218
Nicole Rifkin <nicole.rifkin@simplybusiness.com>
2020-08-27 12:27:45 -0400

    oops

--- test/message_test.rb
+++ test/message_test.rb
@@ -20,14 +20,12 @@ describe Twiglet::Message do
     end
   end
 
-  describe '#format' do
-    it 'returns a message hash from a string' do
-      assert_equal Twiglet::Message.new('hello, world'), { message: 'hello, world' }
-    end
+  it 'returns a message hash from a string' do
+    assert_equal Twiglet::Message.new('hello, world'), { message: 'hello, world' }
+  end
 
-    it 'returns a message hash with symbolized keys' do
-      input_message = { 'key' => 'value', 'message' => 'hello, world' }
-      assert_equal Twiglet::Message.new(input_message), { key: 'value', message: 'hello, world' }
-    end
+  it 'returns a message hash with symbolized keys' do
+    input_message = { 'key' => 'value', 'message' => 'hello, world' }
+    assert_equal Twiglet::Message.new(input_message), { key: 'value', message: 'hello, world' }
   end
 end
